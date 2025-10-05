bb1366c10f4919831beb73ea6dce0897a99a3667
SEM <SEM@mail1.com>
2025-10-05 13:19:42 +0330

    add Week03-hw1.py

--- Week03/hw/hw1.py
+++ Week03/hw/hw1.py
@@ -1,1 +1,29 @@
 
+from itertools import permutations
+import copy
+
+
+words = input("Enter a statement: ")
+
+words_lst = words.split()
+
+def posible():
+    start = 2
+    while start <5:
+        yield list(permutations(words_lst,start))
+        start +=1
+
+lst_pos = posible()
+final_lst=[]
+for word in lst_pos:
+    final_lst.append(word)
+
+
+shallow_copy = copy.copy(final_lst)
+deep_copy = copy.deepcopy(final_lst)
+final_lst[0][0]='maktab137'
+print("shallow_copy".center(100,'*'))
+print(shallow_copy)
+print("deep_copy".center(100,'*'))
+print(deep_copy)
+print(len(final_lst[2]))
