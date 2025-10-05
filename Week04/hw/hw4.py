ccba8249b594412deb93ccd5623d5609638ffd05
SEM <SEM@mail1.com>
2025-10-05 15:14:37 +0330

    add week04-hw4

---
+++ Week04/hw/hw4.py
@@ -0,0 +1,11 @@
+def nested_sum(number_list):
+    sum = 0
+    for item in range(len(number_list)):
+        if type(number_list[item])==list:
+            sum += nested_sum(number_list[item])
+        else:
+            sum +=number_list[item]
+    return sum
+
+list1=[1,[2,3,[4,1]]]
+print(nested_sum(list1))
