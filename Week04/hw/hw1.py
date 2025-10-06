6df1ca23713fffacc2ee3be6c5575a02092b09ee
SEM <SEM@mail1.com>
2025-10-06 19:26:14 +0330

    add Week04-hw1.py

--- Week04/hw/hw1.py
+++ Week04/hw/hw1.py
@@ -1,1 +1,19 @@
+from datetime import datetime
 
+hour = datetime.now().hour
+
+def restrict_hours(start, end):
+    def wrapper(func):
+        def check_time(*args):
+            if start<hour<end:
+                func(*args)
+            else:
+                print("Not Allowed....")
+        return check_time
+    return wrapper
+    
+@restrict_hours(start=9, end=17)
+def do_work():
+    print("working....")
+
+do_work()
