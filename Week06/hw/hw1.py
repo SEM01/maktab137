542141400242264416bbc71b75f79d72a5013f95
S^M <Sem@mes.com>
2025-10-18 11:13:27 -0500

    Add Borrow_book method

--- Week06/hw/hw1.py
+++ Week06/hw/hw1.py
@@ -1,1 +1,19 @@
+class Member:
+    def __init__(self, name: str, member_id: int, email: str):
+        self.name = name
+        self.member_id = member_id
+        self.emial = email
+        self.borrowed_book = []
 
+    def borrow_book(self, book: str):
+        self.book = book
+        self.borrowed_book.append(book)
+
+    def show_borrowed(self):
+        print(self.borrowed_book)
+
+
+a = Member("Ali", 1, "ali@a.com")
+a.borrow_book("binavayan")
+# a.borrow_book("100 sal tanhayie")
+a.show_borrowed()
