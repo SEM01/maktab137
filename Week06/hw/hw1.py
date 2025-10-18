cbfef671537ea5e9e794d0190bd6da1190a0e07a
S^M <Sem@mes.com>
2025-10-18 11:32:37 -0500

    Add show_info method

--- Week06/hw/hw1.py
+++ Week06/hw/hw1.py
@@ -9,17 +9,22 @@ class Member:
         self.book = book
         self.borrowed_book.append(book)
 
-    def show_borrowed(self):
-        print(self.borrowed_book)
-
     def return_book(self, book: str):
         self.book = book
         self.borrowed_book.remove(book)
+
+    def show_info(self):
+        print(
+            f"Member name:{self.name} MemberID:{self.member_id} Member Email:{self.emial}"
+        )
+        print("List of Borrowed Books".center(50, "-"))
+        for i, book in enumerate(self.borrowed_book, start=1):
+            print(f"{i}. |{book}|")
 
 
 a = Member("Ali", 1, "ali@a.com")
 a.borrow_book("binavayan")
 a.borrow_book("100 sal tanhayie")
-a.show_borrowed()
+a.borrow_book("I B4 U")
 a.return_book("binavayan")
-a.show_borrowed()
+a.show_info()
