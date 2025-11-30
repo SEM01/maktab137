440b50e5625f03f1eee1b8268941482d4f7e1bc6
S^M <Sem@mes.com>
2025-11-30 11:38:46 -0600

    update

--- Week12/hw/hw.py
+++ Week12/hw/hw.py
@@ -1,1 +1,25 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Any
+
+
+def id_gen():
+    i = 1
+    while True:
+        yield i
+        i += 1
+
+
+id = id_gen()
+
+
+@dataclass
+class Reminder:
+    title: str
+    time: str
+    reminder_id: int = field(default_factory=lambda: next(id))
+
+
+task1 = Reminder("wake", "10.20")
+print(task1)
+task2 = Reminder("wake", "10.20")
+print(task2)
