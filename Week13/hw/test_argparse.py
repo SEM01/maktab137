c93fef51f3386630f763f577d30cc9908602eaf4
SEM01 <195654711+SEM01@users.noreply.github.com>
2025-12-13 14:43:08 +0100

    Add files via upload

---
+++ Week13/hw/test_argparse.py
@@ -0,0 +1,45 @@
+from t import main
+import pytest
+import shlex
+
+
+test_case = [
+    ("--file='server.log' scan '--ip'", "8.8.8.8"),
+    ("--file='server.log' scan '--url'", "https://example.com/"),
+    ("--file='server.log' scan '--errors'", "404 512"),
+    ("--file='server.log' scan '--ip' '--count'", "15"),
+    ("--file='server.log' scan '--url' '--count'", "8"),
+    ("--file='server.log' scan '--errors' '--count'", "11"),
+    ("--file='server.log' stats", "192.168.10.45"),
+    ("--file='server.log' clean '--api'", "172.16.0.55"),
+    ("--file='server.log' monitor '--error'", "185.123.45.67"),
+]
+
+
+@pytest.mark.parametrize("command, expected_output", test_case)
+def test_argparse(capsys, command, expected_output):
+    main(shlex.split(command))
+    captured = capsys.readouterr()
+    output = captured.out + captured.err
+    assert expected_output in output
+
+
+test_case_sys_exit = [
+    (
+        "--file='server.log' scan",
+        "Arguments are required: --ip",
+    ),
+    (
+        "--file='server.log' sscan '--ip'",
+        "Arguments are required: --ip",
+    ),
+]
+
+
+@pytest.mark.parametrize("command, expected_output", test_case_sys_exit)
+def test_argparse_sys_exit(capsys, command, expected_output):
+    with pytest.raises(SystemExit):
+        main(shlex.split(command))
+    captured = capsys.readouterr()
+    output = captured.out + captured.err
+    assert expected_output in output
