5173d962d1243fc906d43de543f4a493561f4312
SEM01 <195654711+SEM01@users.noreply.github.com>
2025-12-14 15:29:45 +0330

    Update bash_test.sh

--- Week13/hw/bash_test.sh
+++ Week13/hw/bash_test.sh
@@ -2,9 +2,7 @@
 
 log_file="report$(date +%Y-%m-%d).log"
 
-cd "/home/office/Documents/Learning/"
+cd "/home/Learning/"
 source .venv/bin/activate
-cd "/home/office/Documents/Learning/project"
-
 
 pytest -v test_argparse.py >> "$log_file"
